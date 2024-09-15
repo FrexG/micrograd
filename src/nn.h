507363830ea7728b77c3a0f6100ee2dc771cc955
Fraol Gelana <fraolgela@gmail.com>
2024-09-15 16:45:03 +0800

    fix bug on parameter initialization

--- src/nn.h
+++ src/nn.h
@@ -51,6 +51,8 @@ Neuron *initNeuron(size_t num_inputs)
 {
   Neuron *neuron = (Neuron *)calloc(1, sizeof(Neuron));
 
+  double std_dev = sqrt(2.0 / num_inputs);
+
   if (neuron == NULL)
   {
     fprintf(stderr, "Error allocating memory for a neuron\n");
@@ -59,18 +61,15 @@ Neuron *initNeuron(size_t num_inputs)
   neuron->num_inputs = num_inputs;
   neuron->weights = (Value **)calloc(num_inputs, sizeof(Neuron *));
 
-  double data;
-
   for (size_t i = 0; i < num_inputs; ++i)
   {
-    data = (double)rand() / RAND_MAX;
+    // change seed everytime
+    double data = (double)(rand() % RAND_MAX) / RAND_MAX * 2.0 * std_dev - std_dev;
     Value *w = initValue(data);
     neuron->weights[i] = w;
   }
 
-  data = (double)rand() / RAND_MAX;
-  neuron->bias = initValue(data);
-
+  neuron->bias = initValue(0.0);
   return neuron;
 }
 
@@ -100,11 +99,9 @@ Value **forward(Network *net, Value **inpt)
       logit = _add(logit, neuron->bias);
 
       if (layer_id == net->num_layers - 1)
-        logit = _sigmoid(logit);
-        //logit = logit;
+        logit = _tanh(logit);
       else
         logit = _relu(logit);
-        //logit = _tanh(logit);
       logits[neuron_id] = logit;
     }
     // free(activations);
@@ -169,7 +166,7 @@ Value *crossEntropy(Value *logit, Value *target)
 {
   /* This is an implementation of a binary cross entropy loss */
   Value *prior = _mul(target, _log(logit));
-  Value *post = _mul(_scalarAdd(_scalarMul(target, -1), 1), _log(_scalarAdd(_scalarMul( logit, -1), 1)));
+  Value *post = _mul(_scalarAdd(_scalarMul(target, -1), 1), _log(_scalarAdd(_scalarMul(logit, -1), 1)));
   return _add(prior, post);
 }
 
@@ -184,15 +181,15 @@ void sgd(Network *net, double lr, bool bias)
 
       if (bias == true)
         neuron->bias->data -= lr * neuron->bias->grad;
-        // Underflow prevention
-        if(neuron->bias->data < EPSILON) 
-          neuron->bias->data = EPSILON;
+      // Underflow prevention
+      if (neuron->bias->data < EPSILON)
+        neuron->bias->data = EPSILON;
 
       for (size_t i = 0; i < neuron->num_inputs; ++i)
       {
         neuron->weights[i]->data -= lr * neuron->weights[i]->grad;
         // Underflow prevention
-        if(neuron->weights[i]->data < EPSILON)
+        if (neuron->weights[i]->data < EPSILON)
           neuron->weights[i]->data = EPSILON;
       }
     }
